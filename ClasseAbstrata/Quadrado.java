75facf960d03e3c8081f5dbe53f1971c32c960d1
Larissa <143803506+LarissaCardozo@users.noreply.github.com>
2024-08-13 07:25:54 -0300

    Add files via upload

---
+++ ClasseAbstrata/Quadrado.java
@@ -0,0 +1,13 @@
+
+public class Quadrado extends Figura {
+    private double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+}
